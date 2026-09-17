8388f9fcca3e8fb84bbc8bbad8903b1a5fde670b
BelyTr <troillanbelen@gmail.com>
2026-09-17 19:10:08 -0300

    17/9/26

---
+++ src/main/java/org/example/chatws/controller/ChatController.java
@@ -0,0 +1,23 @@
+package org.example.chatws.controller;
+
+import org.example.chatws.entity.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller 
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage enviarMessage(ChatMessage mensaje) {
+        System.out.println("Mensaje Recibido en Servidor:" + mensaje);
+        return mensaje;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage agregarUsuario(ChatMessage mensaje) {
+        System.out.println("Mensaje Recibido en Servidor:" + mensaje);
+        return mensaje;
+    }
+}
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,14 +3,32 @@ let stompClient = null;
 function connect() {
     const serverIp = document.getElementById('serverIp').value;
     
-//conexion y subcripcion
+    const socket = new SockJS("http://" + serverIp + ":8080/chat");
+    stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, function (frame){
+    setConnected(true);
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/topic/public', function (message) {
+        showMessage(JSON.parse(message.body));
+    });
+}, function (error) {
+    console.error('Error connecting to WebSocket:', error);
+    setConnected(false);
+});
+
+    //conexion y subcripcion
 }
 
 function disconnect() {
     // Desconectar
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
     setConnected(false);
     console.log("Disconnected");
 }
+
 
 function setConnected(connected) {
     const status = document.getElementById('status');
@@ -29,6 +47,12 @@ function sendMessage() {
     const content = document.getElementById('message').value;
     
     if (content.trim() === '') return;
+
+    stompClient.send("/app/chat.sendMessage", {}, JSON.stringify({ 
+        tipo: 'CHAT', 
+        usuario: username, 
+        contenido: content 
+    }));
     
      // Conexion
     
